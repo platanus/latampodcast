81ea6a981bde93f61819809329c1a0c39bfe70c6
Stephanie Chau <schau@uc.cl>
2020-03-03 16:50:10 -0300

    :art: Add migration to url to podcast

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_175638) do
+ActiveRecord::Schema.define(version: 2020_03_03_194749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_175638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.string "url", default: ""
   end
 
   create_table "user_podcasts", force: :cascade do |t|
